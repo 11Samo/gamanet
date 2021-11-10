9e6a5ffe8b97f3a57d78cd35ecf3a4f519e1f149
11Samo <samuel.kurnas@gmail.com>
2021-11-10 08:59:40 +0100

    Add to url params from login form

--- notificationHandler.js
+++ notificationHandler.js
@@ -1,16 +1,22 @@
 "use strict";
 
-const onForgotPassword = () => modal("alert-danger", "Forgot Password");
+const onForgotPassword = () => {
+  modal("alert-danger", "Forgot Password");
+};
 const onCreatAccount = () => modal("alert-success", "Create Account");
 
-const onLogin = () => {
+const onLogin = (event) => {
+  event.preventDefault();
   const email = document.getElementById("email-input").value;
-
   const isEmail = email.includes("@");
   const pass = document.getElementById("pass-input").value;
   const isEmpty = pass.length == 0;
 
   if (isEmail && !isEmpty) {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.append("email", email);
+    urlParams.append("pass", pass);
+    window.location.search = urlParams;
     modal("alert-success", `Email:${email}, Password:${pass}`);
   }
 };
